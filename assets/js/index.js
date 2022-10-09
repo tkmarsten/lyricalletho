fb7d2074ced06daf3dd56afa461c603f4e1696ce
victor <vicdotexe@gmail.com>
2022-10-08 23:48:20 -0700

    solid proof of concept working. enter lyrics -> lists possible tracks -> click on track -> gets albumcover/spotify url link

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,8 @@ var elements = {
 
 var lastGeniusData;
 var genius = new Genius("GXl-jB2YMmbsujcZGKUFVHcIzhsZWf3XidKl02rkhtnwjHoWrwNEK8QqsDn73Oje");
+var spotify = new Spotify("3652984d5de34bf48e79cf4623a6d108", "904da68474a34335a798dfef767188ac");
+spotify.initialize();
 
 function onSearchPressed(){
     var input = elements.lyricInput.value;
@@ -37,22 +39,64 @@ function onSearchPressed(){
 }
 
 function createResultDiv(index, artist, song){
-    var div = document.createElement("div");
-    div.setAttribute("class", "border-2 h-20");
-    div.setAttribute("data-geniusHitIndex", index);
-    
-    var p1 = document.createElement("p");
-    p1.innerText = artist;
+    var div = document.createElement("button");
+    div.setAttribute("class", "border-2 h-20 bg-orange-400 pointer-events-auto");
+    div.setAttribute("data-genius", index);
+    div.innerText = `${song} - ${artist}`
+    // var p1 = document.createElement("p");
+    // p1.innerText = artist;
 
-    var p2 = document.createElement("p");
-    p2.innerText = song;
+    // var p2 = document.createElement("p");
+    // p2.innerText = song;
 
-    div.appendChild(p1);
-    div.appendChild(p2);
+    // div.appendChild(p1);
+    // div.appendChild(p2);
 
     return div;
 }
 
+function onResultsClick(event){
+    var hitIndex = event.target.getAttribute("data-genius");
+    console.log(hitIndex);
 
+    if (!hitIndex){
+        return;
+    }
+
+    var result = lastGeniusData.response.hits[hitIndex].result;
+    console.log (result);
+
+    var parameters = {
+        q: `${result.title}`,
+        type: 'track'
+    }
+    spotify.get("/search", parameters).then((response)=>{
+        console.log(response);
+        return response.json();
+    }).then((data)=>{
+        console.log(data);
+        loadCoverAndLink(data);
+    })
+
+}
+
+function loadCoverAndLink(data){
+    var firstTrack = data.tracks.items[0];
+    var albumCover = firstTrack.album.images[1].url;
+    var link = firstTrack.external_urls.spotify;
+
+    var a = document.createElement("a");
+    a.setAttribute("href", link);
+    a.setAttribute("target", "_blank");
+    
+    var img = document.createElement("img");
+    img.setAttribute("src", albumCover);
+    
+    a.appendChild(img);
+
+    elements.bottomBox.innerHTML = "";
+    elements.bottomBox.appendChild(a);
+}
 
 elements.searchButton.addEventListener("click", onSearchPressed);
+elements.resultsContainer.addEventListener("click", onResultsClick);
